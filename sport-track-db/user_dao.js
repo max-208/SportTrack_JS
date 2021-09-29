97e0c70d6b7d374a234f5eccc67de7a0f51278cd
WiPhil <philo102002@gmail.com>
2021-09-29 09:23:53 +0200

    user_dao.js

--- sport-track-db/user_dao.js
+++ sport-track-db/user_dao.js
@@ -3,10 +3,27 @@ var UserDAO = function(){
     this.insert = function(values, callback){
         
     };
-    this.update = function(key, values, callback){...};
-    this.delete = function(key, callback){...};
-    this.findAll = function(callback){...};
-    this.findByKey = function(key, callback){...};
+    this.update = function(key, values, callback){
+        
+    };
+    this.delete = function(key, callback){
+        
+        var query = "delete from user where Email = ? ";
+        var stmt = db.prepare(query);
+        stmt.run(key);
+        //try{
+            stmt.finalize();   
+        //} catch(e){
+        //    console.log( "User_dao delete : exception recue : "+e.getMessage() );
+        //}
+    };
+
+    this.findAll = function(callback){
+
+    };
+    this.findByKey = function(key, callback){
+        
+    };
 };
 var dao = new UserDAO();
 module.exports = dao;
