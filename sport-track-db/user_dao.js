0f4642bc9a2dd3899a91b9fa3adb3c9867ec8f34
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-04 10:05:57 +0200

    userDAO

--- sport-track-db/user_dao.js
+++ sport-track-db/user_dao.js
@@ -1,42 +1,28 @@
-var db = require('./sqlite_connection');
+const db = require('./sqlite_connection');
 var UserDAO = function(){
-    this.insert = function(values, callback){
-        let statement = db.prepare("insert into user ( Email, Name, Surname, BirthDate, Gender, Height, Weight, Password ) values (?, ?, ?, ?, ?, ?, ?, ?)")
-        statement.run(values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"])
-        statement.finalize();
-    };
-    this.update = function(key, values, callback){
-        let statement = db.prepare("update user set Email = ?, Name = ?, Surname = ?, BirthDate = ?, Gender = ?, Height = ?, Weight = ?, Password = ? where Email = ?")
-        statement.run(values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"], values["oldEmail"])
-        statement.finalize();
-    };
-    this.delete = function(key, callback){
-        
-        var query = "delete from user where Email = ? ";
-        var stmt = db.prepare(query);
-        stmt.run(key);
-        //try{
-            stmt.finalize(callback);   
-        //} catch(e){
-        //    console.log( "User_dao delete : exception recue : "+e.getMessage() );
-        //}
-    };
 
     this.findAll = function(callback){
-        var query = "select * from user ";
-        var stmt = db.prepare(query);
-        stmt.get();
-        stmt.finalize(callback); 
-
-
+        const query = "select * from user ";
+        db.all(query,[],callback);
+    };
+    
+    this.insert = function(values, callback){
+        const querry = "insert into user ( Email, Name, Surname, BirthDate, Gender, Height, Weight, Password ) values (?, ?, ?, ?, ?, ?, ?, ?)";
+        db.all(querry,[values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"]],callback);
+    };
+    this.update = function(key, values, callback){
+        const querry = "update user set Email = ?, Name = ?, Surname = ?, BirthDate = ?, Gender = ?, Height = ?, Weight = ?, Password = ? where Email = ?";
+        db.all(querry,[values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"], values["oldEmail"]],callback);
+    };
+    this.delete = function(key, callback){
+        const query = "delete from user where Email = ? ";
+        db.all(query,[key],callback);
     };
     this.findByKey = function(key, callback){
-        var query = "select * from user where Email = ? ";
-        var stmt = db.prepare(query);
-        stmt.get(key);
-        stmt.finalize(callback); 
+        const query = "select * from user where Email = ? ";
+        db.all(query,[key],callback); 
         
     };
 };
-var dao = new UserDAO();
+const dao = new UserDAO();
 module.exports = dao;
