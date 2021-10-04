7d583835c56a7dff3263f9c3cc8468a5e6cbcac3
WiPhil <philo102002@gmail.com>
2021-10-04 10:17:39 +0200

    activity_dao

---
+++ sport-track-db/activity_dao.js
@@ -0,0 +1,33 @@
+const db = require('./sqlite_connection');
+var ActivityDAO = function(){
+
+    this.findAll = function(callback){
+        const query = "select * from activity ";
+        db.all(query,[],callback);
+    };
+
+    this.findforUser = function(callback){
+        const query = "select * from activity, user where TheUser = Email ";
+        db.all(query,[],callback);
+    };
+    
+    this.insert = function(values, callback){
+        const querry = "insert into activity ( IdActivity, Date, Description, TheUser, MaxCardio, MinCardio, AverageCardio, BegginingTime, Duration ) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        db.all(querry,[values["IdActivity"], values["Date"], values["Description"], values["TheUser"], values["MaxCardio"], values["MinCardio"], values["AverageCardio"], values["BegginingTime"],values["Duration"]],callback);
+    };
+    this.update = function(key, values, callback){
+        const querry = "update activity set IdActivity = ?, Date = ?, Description = ?, TheUser = ?, MaxCardio = ?, MinCardio = ?, AverageCardio = ?, BegginingTime = ? where Duration = ?";
+        db.all(querry,[values["IdActivity"], values["Date"], values["Description"], values["TheUser"], values["MaxCardio"], values["MinCardio"], values["AverageCardio"], values["BegginingTime"], values["Duration"]],callback);
+    };
+    this.delete = function(key, callback){
+        const query = "delete from activity where IdActivity = ? ";
+        db.all(query,[key],callback);
+    };
+    this.findByKey = function(key, callback){
+        const query = "select * from activity where IdActivity = ? ";
+        db.all(query,[key],callback); 
+        
+    };
+};
+const dao = new ActivityDAO();
+module.exports = dao;
