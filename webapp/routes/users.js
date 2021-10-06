170d3642beee18b5236f83f1b70b911138f64b93
WiPhil <philo102002@gmail.com>
2021-10-06 09:25:34 +0200

    users.js users.jade creationUser

--- webapp/routes/users.js
+++ webapp/routes/users.js
@@ -8,6 +8,27 @@ router.get('/', function(req, res, next) {
     }else {
       res.render('users', {data:rows});
     }
-  });
+
+});
+    let user = 
+    {"Email" : "BD@mail.com"
+    ,"Name" : "Birth"
+    ,"Surname" : "Date"
+    ,"BirthDate" : "01/12/2000"
+    ,"Gender" : "Homme"
+    ,"Height" : "140"
+    ,"Weight" : "58"
+    ,"Password" : "1234CaRRottes€"
+    }
+    user_dao.insert(user,function(err, rows) {
+        if(err != null){
+          console.log("ERROR= " +err);
+        }else {
+          res.render('users', {data:rows});
+        }
+    });
+
+
+  
 });
 module.exports = router;
