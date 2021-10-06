d60c42b8b3457a9ba4b9f2a1a687ee022522e2c7
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-06 08:11:47 +0200

    resolution bug userDAO

--- sport-track-db-test.js
+++ sport-track-db-test.js
@@ -12,39 +12,65 @@ async function testFunc(){
     ,"Weight" : "1"
     ,"Password" : "1234CaRRottes€"
     }
-    function callback(err,rows){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(rows);
-        }
-    }
     
-    await user_dao.insert(test,callback);
-    await user_dao.findAll(callback);
-    await user_dao.findByKey("test@mail.com",callback);
-    await user_dao.update("test@mail.com",test, callback);
-    await user_dao.findByKey("test@mail.com",callback);
-    await user_dao.delete("test@mail.com",callback);
-    await user_dao.findAll(callback);
-    let testAcitivty =
-    {"IdActivity" : "1"
-    ,"Date" : "01/01/2000"
-    ,"Description" : "test1"
-    ,"TheUser" : "test@mail.com"
-    ,"MaxCardio" : "100"
-    ,"MinCardio" : "100"
-    ,"AverageCardio" : "100"
-    ,"BegginingTime" : "00:00:00"
-    ,"Duration" : "00:00:00"
-    }
-    await activity_dao.insert(testAcitivty,callback);
-    await activity_dao.findAll(callback);
-    await activity_dao.findByKey("1",callback);
-    await activity_dao.update("1",testAcitivty, callback);
-    await activity_dao.findByKey("1",callback);
-    await activity_dao.delete("1",callback);
-    await activity_dao.findAll(callback);
-}
+    await user_dao.insert(test).then((value)=>console.log(value));
+    await user_dao.findAll().then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.update("test@mail.com",test).then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    await user_dao.delete("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.findAll().then((value)=>console.log(value));
+    
+    await user_dao.insert(test).then((value)=>console.log(value));
+    await user_dao.findAll().then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.update("test@mail.com",test).then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    await user_dao.delete("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.findAll().then((value)=>console.log(value));
+    
+    await user_dao.insert(test).then((value)=>console.log(value));
+    await user_dao.findAll().then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.update("test@mail.com",test).then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    await user_dao.delete("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.findAll().then((value)=>console.log(value));
+    
+    await user_dao.insert(test).then((value)=>console.log(value));
+    await user_dao.findAll().then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.update("test@mail.com",test).then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    await user_dao.delete("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.findAll().then((value)=>console.log(value));
+    
+    await user_dao.insert(test).then((value)=>console.log(value));
+    await user_dao.findAll().then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.update("test@mail.com",test).then((value)=>console.log(value));
+    //await user_dao.findByKey("test@mail.com").then((value)=>console.log(value));
+    await user_dao.delete("test@mail.com").then((value)=>console.log(value));
+    //await user_dao.findAll().then((value)=>console.log(value));
+
+    //let testAcitivty =
+    //{"IdActivity" : "1"
+    //,"Date" : "01/01/2000"
+    //,"Description" : "test1"
+    //,"TheUser" : "test@mail.com"
+    //,"MaxCardio" : "100"
+    //,"MinCardio" : "100"
+    //,"AverageCardio" : "100"
+    //,"BegginingTime" : "00:00:00"
+    //,"Duration" : "00:00:00"
+    //}
+    //await activity_dao.insert(testAcitivty);
+    //await activity_dao.findAll();
+    //await activity_dao.findByKey("1");
+    //await activity_dao.update("1",testAcitivty);
+    //await activity_dao.findByKey("1");
+    //await activity_dao.delete("1");
+    //await activity_dao.findAll();
+}//
 
 testFunc();
--- sport-track-db/user_dao.js
+++ sport-track-db/user_dao.js
@@ -1,22 +1,37 @@
 const db = require('./sqlite_connection');
 var UserDAO = function(){
 
-    this.findAll = function(callback){
-        const query = "select * from user ";
-        db.all(query,[],callback);
+    this.findAll = async function(){
+        return new Promise(async function(resolve,reject){
+            const query = "select * from user ";
+            db.all(query,[],(err,rows)=>{
+                if(err)reject(err);
+                resolve(rows)
+            });
+        });
     };
     
-    this.insert = function(values, callback){
-        const querry = "insert into user ( Email, Name, Surname, BirthDate, Gender, Height, Weight, Password ) values (?, ?, ?, ?, ?, ?, ?, ?)";
-        db.all(querry,[values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"]],callback);
+    this.insert = async function(values){
+        return new Promise(async function(resolve,reject){
+            const querry = "insert into user ( Email, Name, Surname, BirthDate, Gender, Height, Weight, Password ) values (?, ?, ?, ?, ?, ?, ?, ?)";
+            db.run(querry,[values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"]],(err,rows)=>{
+                if(err)reject(err);
+                resolve(rows)
+            });
+        });
     };
     this.update = function(key, values, callback){
         const querry = "update user set Email = ?, Name = ?, Surname = ?, BirthDate = ?, Gender = ?, Height = ?, Weight = ?, Password = ? where Email = ?";
         db.all(querry,[values["Email"], values["Name"], values["Surname"], values["BirthDate"], values["Gender"], values["Height"], values["Weight"], values["Password"], key],callback);
     };
-    this.delete = function(key, callback){
-        const query = "delete from user where Email = ? ";
-        db.all(query,key,callback);
+    this.delete = async function(key, callback){
+        return new Promise(async function(resolve,reject){
+            const query = "delete from user where Email = ? ";
+            db.run(query,key,(err,rows)=>{
+                if(err)reject(err);
+                resolve(rows)
+            });
+        });
     };
     this.findByKey = function(key, callback){
         const query = "select * from user where Email = ? ";
