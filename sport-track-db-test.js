986cb8bf4c126c6527e8589e22ccc8add3129552
WiPhil <philo102002@gmail.com>
2021-10-06 08:15:05 +0200

    Test

--- sport-track-db-test.js
+++ sport-track-db-test.js
@@ -12,6 +12,17 @@ async function testFunc(){
     ,"Weight" : "1"
     ,"Password" : "1234CaRRottes€"
     }
+    let test2=
+    {"Email" : "test@mail.com"
+    ,"Name" : "test2"
+    ,"Surname" : "test2"
+    ,"BirthDate" : "01/01/2000"
+    ,"Gender" : "Autres"
+    ,"Height" : "1"
+    ,"Weight" : "1"
+    ,"Password" : "1234CaRRottes€"
+    }
+
     function callback(err,rows){
         if(err){
             console.log(err);
@@ -20,13 +31,18 @@ async function testFunc(){
         }
     }
     
+
+    
     await user_dao.insert(test,callback);
     await user_dao.findAll(callback);
+    
     await user_dao.findByKey("test@mail.com",callback);
-    await user_dao.update("test@mail.com",test, callback);
+    await user_dao.update("test@mail.com",test2, callback);
     await user_dao.findByKey("test@mail.com",callback);
     await user_dao.delete("test@mail.com",callback);
     await user_dao.findAll(callback);
+
+    /*
     let testAcitivty =
     {"IdActivity" : "1"
     ,"Date" : "01/01/2000"
@@ -36,15 +52,29 @@ async function testFunc(){
     ,"MinCardio" : "100"
     ,"AverageCardio" : "100"
     ,"BegginingTime" : "00:00:00"
-    ,"Duration" : "00:00:00"
+    ,"Duration" : "00:00:10"
+    }
+    let testAcitivty2 =
+    {"IdActivity" : "1"
+    ,"Date" : "01/01/2000"
+    ,"Description" : "test1"
+    ,"TheUser" : "test@mail.com"
+    ,"MaxCardio" : "110"
+    ,"MinCardio" : "100"
+    ,"AverageCardio" : "100"
+    ,"BegginingTime" : "00:00:00"
+    ,"Duration" : "00:00:10"
     }
     await activity_dao.insert(testAcitivty,callback);
     await activity_dao.findAll(callback);
     await activity_dao.findByKey("1",callback);
-    await activity_dao.update("1",testAcitivty, callback);
+    await activity_dao.update("1",testAcitivty2, callback);
     await activity_dao.findByKey("1",callback);
     await activity_dao.delete("1",callback);
     await activity_dao.findAll(callback);
+    */
+    
+
 }
 
 testFunc();
