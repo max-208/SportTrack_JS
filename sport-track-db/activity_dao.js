c54b7e6326d60d294b7c6185dd1311bad2a12d98
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-06 08:36:13 +0200

    tests activityDAO

--- sport-track-db/activity_dao.js
+++ sport-track-db/activity_dao.js
@@ -1,7 +1,17 @@
 const db = require('./sqlite_connection');
 var ActivityDAO = function(){
 
-    this.findAll = function(callback){
+    this.insert = function(values){
+        return new Promise(async function(resolve,reject){
+            const query = "insert into activity (IdActivity, Date, Description, TheUser, MaxCardio, MinCardio, AverageCardio, BegginingTime,  Duration ) values (?, ?, ?, ?, ?, ?, ?, ?, ? )";
+            db.run(query,[values["IdActivity"], values["Date"], values["Description"], values["TheUser"], values["MaxCardio"], values["MinCardio"], values["AverageCardio"], values["BegginingTime"], values["Duration"]],(err,rows)=>{
+                if(err)reject(err)
+                resolve(rows)
+            });
+        });
+    };
+
+    this.findAll = function(){
         return new Promise(async function(resolve,reject){
             const query = "select * from activity ";
             db.all(query,[],(err,rows)=>{
@@ -9,14 +19,12 @@ var ActivityDAO = function(){
                 resolve(rows)
             });
         });
-    
-
     };
 
-    this.findforUser = function(key,callback){
+    this.findforUser = function(key){
         return new Promise(async function(resolve,reject){
-            const query = "select * from activity, user where TheUser = Email ";
-            db.all(query,[values["IdActivity"], values["Date"], values["Description"], values["TheUser"], values["MaxCardio"], values["MinCardio"], values["AverageCardio"], values["BegginingTime"],values["Duration"]],(err,rows)=>{
+            const query = "select * from activity, user where TheUser = ? ";
+            db.all(query,[key],(err,rows)=>{
                 if(err)reject(err)
                 resolve(rows)
             });
@@ -25,26 +33,28 @@ var ActivityDAO = function(){
 
     };
 
-    this.update = function(key, values, callback){
+    this.update = function(key, values){
         return new Promise(async function(resolve,reject){
-            const querry = "update activity set IdActivity = ?, Date = ?, Description = ?, TheUser = ?, MaxCardio = ?, MinCardio = ?, AverageCardio = ?, BegginingTime = ?,  Duration = ? where IdActivity = ?";
-            db.all(query,[values["IdActivity"], values["Date"], values["Description"], values["TheUser"], values["MaxCardio"], values["MinCardio"], values["AverageCardio"], values["BegginingTime"], values["Duration"], key],(err,rows)=>{
+            const query = "update activity set IdActivity = ?, Date = ?, Description = ?, TheUser = ?, MaxCardio = ?, MinCardio = ?, AverageCardio = ?, BegginingTime = ?,  Duration = ? where IdActivity = ?";
+            db.run(query,[values["IdActivity"], values["Date"], values["Description"], values["TheUser"], values["MaxCardio"], values["MinCardio"], values["AverageCardio"], values["BegginingTime"], values["Duration"], key],(err,rows)=>{
                 if(err)reject(err)
                 resolve(rows)
             });
         });
     };
-    this.delete = function(key, callback){
+
+    this.delete = function(key){
         return new Promise(async function(resolve,reject){
             const query = "delete from activity where IdActivity = ? ";
-            db.all(query,key,(err,rows)=>{
+            db.run(query,key,(err,rows)=>{
                 if(err)reject(err)
                 resolve(rows)
             });
         });
 
     };
-    this.findByKey = function(key, callback){
+
+    this.findByKey = function(key){
         return new Promise(async function(resolve,reject){
             const query = "select * from activity where IdActivity = ? ";
             db.all(query,key,(err,rows)=>{ 
