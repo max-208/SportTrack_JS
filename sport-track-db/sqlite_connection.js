6432a582bffd82ea8a8cdc95bd4e74dfe9cfc02d
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-11 10:38:18 +0200

    promise user.js

--- sport-track-db/sqlite_connection.js
+++ sport-track-db/sqlite_connection.js
@@ -2,4 +2,4 @@ const sqlite3 = require('sqlite3').verbose();
 function initializeConnection(filename){
     return new sqlite3.Database(filename);
 }
-module.exports = initializeConnection("sport-track-db/sport_track.db");
+module.exports = initializeConnection("../sport-track-db/sport_track.db");
--- webapp/routes/users.js
+++ webapp/routes/users.js
@@ -3,30 +3,28 @@ var router = express.Router();
 var user_dao = require('sport-track-db').user_dao;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 
 
 router.get('/', function(req, res, next) {
-  user_dao.findAll(function(err, rows) {
+  user_dao.findAll()
+  .then((rows,err) => {
     if(err != null){
       console.log("ERROR= " +err);
     }else {
       res.render('users', {data:rows});
     }
-    });
+  })
 });
   
-  router.insert(user, function(req, res, next) {
-    user_dao.insert(function(err, rows) {
+  router.get("/insert", function(req, res, next) {
+    user_dao.insert(user)
+    .then((rows,err) => {
       if(err != null){
         console.log("ERROR= " +err);
       }else {
         res.render('users', {data:rows});
       }
-    });
+    })
   });
 module.exports = router;
   
