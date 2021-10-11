63e09a364d8bd9c1199956dfc31b7a249ece3bda
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-11 11:12:36 +0200

    fonctionnement de la view user

--- webapp/routes/users.js
+++ webapp/routes/users.js
@@ -4,20 +4,13 @@ var user_dao = require('sport-track-db').user_dao;
 
 /* GET users listing. */
 
-
 router.get('/', function(req, res, next) {
-  user_dao.findAll()
-  .then((rows,err) => {
-    if(err != null){
-      console.log("ERROR= " +err);
-    }else {
-      res.render('users', {data:rows});
-    }
-  })
+  res.render("users")
 });
-  
-  router.get("/insert", function(req, res, next) {
-    user_dao.insert(user)
+
+router.post('/', function(req, res, next) {
+  console.log(req.query);
+  user_dao.insert(req.query)
     .then((rows,err) => {
       if(err != null){
         console.log("ERROR= " +err);
@@ -25,6 +18,6 @@ router.get('/', function(req, res, next) {
         res.render('users', {data:rows});
       }
     })
-  });
+});
 module.exports = router;
   
